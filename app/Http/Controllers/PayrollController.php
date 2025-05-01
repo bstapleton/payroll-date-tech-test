88c8515eba1b0419ba7031b1207ae6a0e658f906
Ben Stapleton <webdev.ben@gmail.com>
2025-05-01 12:04:41 +0100

    Set up API dedicated routes and controller method

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -6,43 +6,12 @@ use Illuminate\Http\Request;
 
 class PayrollController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function check(Request $request)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            'data' => [
+                'status' => 'success'
+            ]
+        ]);
     }
 }
